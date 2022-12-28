4f8e7d46c5f5649b63ab2d610f749f1ab5829b9f
Clapton <corioste@kahero.co>
2022-12-28 14:49:47 +0800

    fixed: about

--- bot.py
+++ bot.py
@@ -42,7 +42,7 @@ def about(update, context):
 
 
 def contact(update, context):
-    update.message.reply_text("Developer: Clapton Orioste \nEmail: clapton.five@gmail.com\nPorfolio: claptonorioste.com")
+    update.message.reply_text("Developer: Clapton Orioste \nEmail: clapton.five@gmail.com\nPortfolio: claptonorioste.com")
 
 
 def handle_message(update, context):
